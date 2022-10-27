98e24f21cd4c6ba6dc1a9710fb58243eda6cbc9d
gregcsokas <gergo.csokas@gmail.com>
2022-10-27 20:45:23 +0200

    Custom Text button added

---
+++ templates/text_button/text_button.gd
@@ -0,0 +1,75 @@
+extends BaseButton
+
+@export var button_text = "Button Text"
+
+@export_color_no_alpha var text_color : Color = Color("ffffff")
+@export_color_no_alpha var text_hover_color : Color = Color("006a80")
+@export_color_no_alpha var text_pressed_color : Color = Color("006a80")
+
+@export_color_no_alpha var base_color : Color = Color("5b7980")
+@export_color_no_alpha var hover_color : Color = Color("006a80")
+
+@export_range(0.0, 1.0) var base_alpha : float = 0.5
+@export_range(0.0, 1.0) var hover_alpha : float = 0.5
+
+@onready var color_rect : Node = get_node("ColorRect")
+@onready var label : Node = get_node("Label")
+
+
+func _ready():
+	label.text = button_text
+	label.set("theme_override_colors/font_color", Color(text_color.r, text_color.g, text_color.b, base_alpha))
+	color_rect.color = Color(base_color.r, base_color.g, base_color.b, base_alpha)
+
+
+func _on_text_button_mouse_entered() -> void:
+	var tween = create_tween().set_trans(Tween.TRANS_CIRC).set_parallel(true)
+		
+	tween.tween_property(color_rect,
+		"modulate",
+		Color(hover_color.r, hover_color.g, hover_color.b, hover_alpha),
+		0.25
+		).set_ease(Tween.EASE_IN_OUT)
+		
+	tween.tween_property(label,
+		"modulate",
+		Color(text_hover_color.r, text_hover_color.g, text_hover_color.b, hover_alpha),
+		0.25
+		).set_ease(Tween.EASE_IN_OUT)
+
+
+func _on_text_button_mouse_exited() -> void:
+	var tween = create_tween().set_trans(Tween.TRANS_CIRC).set_parallel(true)
+
+	tween.tween_property(color_rect,
+		"modulate",
+		Color(base_color.r, base_color.g, base_color.b, base_alpha),
+		0.50
+		).set_ease(Tween.EASE_IN_OUT)
+		
+	tween.tween_property(label,
+		"modulate",
+		Color(text_color.r, text_color.g, text_color.b, base_alpha),
+		0.50
+		).set_ease(Tween.EASE_IN_OUT)
+		
+
+func _on_button_down() -> void:
+	var tween = create_tween().set_trans(Tween.TRANS_CIRC)
+	
+	tween.tween_property(label,
+		"modulate",
+		Color(text_pressed_color.r, text_pressed_color.g, text_pressed_color.b, hover_alpha),
+		0.0
+		).set_ease(Tween.EASE_IN_OUT)
+
+
+func _on_button_up() -> void:
+	var tween = create_tween().set_trans(Tween.TRANS_CIRC)
+	
+	if is_hovered():
+		tween.tween_property(label,
+			"modulate",
+			Color(text_hover_color.r, text_hover_color.g, text_hover_color.b, hover_alpha),
+			0.0
+			).set_ease(Tween.EASE_IN_OUT)
